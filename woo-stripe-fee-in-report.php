9b508f508a8e58e0873b418d9e0749690c35b1ba
Nicolas Gehin <nicolas@rvola.com>
2018-10-02 13:51:37 +0200

    ✔︎ Compatibility WooCommerce 3.5

--- woo-stripe-fee-in-report.php
+++ woo-stripe-fee-in-report.php
@@ -18,8 +18,8 @@
  * Requires at least:       4.4
  * Tested up to:            4.9
  * Requires PHP:            5.3
- * WC requires at least:    3.1.0
- * WC tested up to:         3.4.0
+ * WC requires at least:    3.1
+ * WC tested up to:         3.5
  *
  * License:                 GNU General Public License v3.0
  * License URI:             https://www.gnu.org/licenses/gpl-3.0.html

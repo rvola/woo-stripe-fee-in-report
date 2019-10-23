238db5b0ccbd2b9bea642333f698422ceea9933a
Nicolas <nicolas@rvola.com>
2019-10-23 13:54:26 +0200

    FIX / Load languages files

--- includes/class-wp.php
+++ includes/class-wp.php
@@ -55,8 +55,7 @@ class WP {
 	 */
 	public function load_languages() {
 
-		load_plugin_textdomain( 'woo-stripe-fee-in-report', false, dirname( __FILE__ ) . '/languages' );
-		load_plugin_textdomain( 'woo-stripe-fee-in-report', false, plugin_basename( WOOSFR_FILE ) . '/languages' );
+		load_plugin_textdomain( 'woo-stripe-fee-in-report', false, plugin_basename( dirname( WOOCAO_FILE ) ) . '/languages' );
 	}
 
 	/**
